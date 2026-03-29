df01677af33fc24178f1214b3745007a2d546159
mvsr1729 <subbushop1729@gmail.com>
2026-03-29 16:38:34 +0000

    all the linker files before experimenting

--- globals.c
+++ globals.c
@@ -2,7 +2,7 @@
 //int uninit_global;
 //int init_global = 10;
 int abc;
-int pqr = 100;
+volatile const int pqr = 100;
 
 int bar () {
     return 100;
--- main.c
+++ main.c
@@ -3,10 +3,40 @@
 //extern int init_global;
 
 
-int uninit_global;
+int __attribute__((section("abc"))) uninit_global;
 int init_global = 10;
+
+extern char _src_start_data_;
+extern char _dst_start_data_;
+extern char _dst_end_data_;
+extern char _start_bss_;
+extern char _end_bss_;
 
 
 int foo(int local) {
     return local + uninit_global + init_global;
+}
+
+void __attribute__((section("pqr"))) xyz() {
+    char * src_start = &_src_start_data_;
+    char * dst_start = &_dst_start_data_;
+    char * dst_end = &_dst_end_data_;
+    char * start_bss = &_start_bss_;
+    char * end_bss = &_end_bss_;
+
+    while (dst_start != dst_end)
+    {
+        *dst_start = *src_start;
+        dst_start++;
+        src_start++;
+    }
+
+    while (start_bss != end_bss)
+    {
+        *start_bss = 0;
+        start_bss++;
+    }
+    
+
+
 }
